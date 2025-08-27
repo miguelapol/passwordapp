5d135a7313db5fe573e7a18581e86a04cc396855
Miguel Angel <33709574+miguelapol@users.noreply.github.com>
2025-08-26 23:20:31 -0600

    creacion de menu deslizante

--- app/src/main/java/com/example/passwordapp/MainActivity.java
+++ app/src/main/java/com/example/passwordapp/MainActivity.java
@@ -1,14 +1,60 @@
 package com.example.passwordapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.passwordapp.Fragmentos.F_Ajustes;
+import com.example.passwordapp.Fragmentos.F_Todas;
+import com.google.android.material.navigation.NavigationView;
 
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar= findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer= findViewById(R.id.drawer_layout);
+        NavigationView navigationView=findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setItemIconTintList(null);
+
+        ActionBarDrawerToggle toogle= new ActionBarDrawerToggle(this,drawer,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toogle);
+        toogle.syncState();
+
+        /*Fragmento al ejectar la aplicacion*/
+        if(savedInstanceState==null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new F_Todas()).commit();
+            navigationView.setCheckedItem(R.id.Opcion_Todas);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id= item.getItemId();
+
+        if(id==R.id.Opcion_Todas){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new F_Todas()).commit();
+        }
+        if (id==R.id.Opcion_Ajustes){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new F_Ajustes()).commit();
+        }
+        if(id==R.id.Opcion_Salir){
+            Toast.makeText(this, "Cerrar Sesion", Toast.LENGTH_SHORT).show();
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return  true;
     }
 }
